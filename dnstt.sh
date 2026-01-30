02b2db26555322c41f6392bfc1880d70209c58e8
NDEVXLK <ndevslk@gmail.com>
2026-01-30 06:30:42 +0530

    Update dnstt.sh

--- dnstt.sh
+++ dnstt.sh
@@ -1,3 +1,3 @@
-sudo wget -q -O /usr/local/bin/dnstt https://raw.githubusercontent.com/ndevxlk/dnstt/main/dnstt
+sudo wget -4 -q -O /usr/local/bin/dnstt https://raw.githubusercontent.com/ndevxlk/dnstt/main/dnstt
 sudo chmod +x /usr/local/bin/dnstt
 dnstt
